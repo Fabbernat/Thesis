fbb7265d06d65cb24885619de7b7eb482f4c30d2
Fabian Bernat <fabbernat@gmail.com>
2025-02-05 17:12:09 +0100

    Revert "    vectorizer = TfidfVectorizer(lowercase=True, stop_words="english")"
    This reverts commit 985a9d16

--- test_the_WordSenseDisambiguator.py
+++ test_the_WordSenseDisambiguator.py
@@ -1,3 +1,4 @@
-import WordSenseDisambiguator
+from WordSenseDisambiguator import WordSenseDisambiguator
 
-WordSenseDisambiguator.build_sentence('','',0,0,'','')
+wsd = WordSenseDisambiguator()
+build_sentence()
