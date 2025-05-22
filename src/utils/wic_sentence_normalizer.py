dcfbae87b697518cb2cfc45c436ba1dcd1706357
Fabian Bernat <fabbernat@gmail.com>
2025-05-22 17:55:48 +0200

    v7.0.0 fix sentence_normalizer wrong param

--- src/utils/wic_sentence_normalizer.py
+++ src/utils/wic_sentence_normalizer.py
@@ -25,14 +25,15 @@ def make_sentence_human_readable_old(sentence: str) -> str:
     Original implementation with explicit replacements.
     """
     # Contractions
-    sentence = sentence.replace(" 's", "'s")
-    sentence = sentence.replace(" 't", "'t")
-    sentence = sentence.replace(" 'd", "'d")
-    sentence = sentence.replace(" 'm", "'m")
-    sentence = sentence.replace(" 've", "'ve")
-    sentence = sentence.replace(" 'll", "'ll")
-    sentence = sentence.replace(" 're", "'re")
-    sentence = sentence.replace(" n't", "n't")
+    sentence = sentence.replace(" 's", "\\'s")
+    sentence = sentence.replace(" 't", "\\'t")
+    sentence = sentence.replace(" 'd", "\\'d")
+    sentence = sentence.replace(" 'm", "\\'m")
+    sentence = sentence.replace(" 've", "\\'ve")
+    sentence = sentence.replace(" 'll", "\\'ll")
+    sentence = sentence.replace(" 're", "\\'re")
+    sentence = sentence.replace(" n't", "n\\'t")
+    sentence = sentence.replace("o'clock", "o\\'clock")
 
     # Punctuation
     sentence = sentence.replace(" ,", ",")
@@ -41,14 +42,14 @@ def make_sentence_human_readable_old(sentence: str) -> str:
     sentence = sentence.replace(" !", "!")
 
     # Special uppercase cases
-    sentence = sentence.replace("Do n't", "Don't")
-    sentence = sentence.replace("Wo n't", "Won't")
-    sentence = sentence.replace("Ca n't", "Can't")
+    sentence = sentence.replace("Do n't", "Don\\'t")
+    sentence = sentence.replace("Wo n't", "Won\\'t")
+    sentence = sentence.replace("Ca n't", "Can\\'t")
 
     # Special lowercase cases
-    sentence = sentence.replace("do n't", "don't")
-    sentence = sentence.replace("wo n't", "won't")
-    sentence = sentence.replace("ca n't", "can't")
+    sentence = sentence.replace("do n't", "don\\'t")
+    sentence = sentence.replace("wo n't", "won\\'t")
+    sentence = sentence.replace("ca n't", "can\\'t")
 
     # General n't replacement
     sentence = sentence.replace(" n't", "not")
@@ -63,25 +64,25 @@ def make_sentence_human_readable_new(sentence: str) -> str:
     """
     # Contractions mapping
     contractions = {
-        r" 's": "'s",
-        r" 't": "'t",
-        r" 'd": "'d",
-        r" 'm": "'m",
-        r" 've": "'ve",
-        r" 'll": "'ll",
-        r" 're": "'re",
-        r" n't": "n't",
-        r"o' ": "o'",
+        r" 's": "\\'s",
+        r" 't": "\\'t",
+        r" 'd": "\\'d",
+        r" 'm": "\\'m",
+        r" 've": "\\'ve",
+        r" 'll": "\\'ll",
+        r" 're": "\\'re",
+        r" n't": "n\\'t",
+        r"\bo'(?=\w)": r"o\\'"
     }
 
     # Special cases for n't
     special_contractions = {
-        r"Do n't": "Don't",
-        r"Wo n't": "Won't",
-        r"Ca n't": "Can't",
-        r"do n't": "don't",
-        r"wo n't": "won't",
-        r"ca n't": "can't",
+        r"Do n't": "Don\\'t",
+        r"Wo n't": "Won\\'t",
+        r"Ca n't": "Can\\'t",
+        r"do n't": "don\\'t",
+        r"wo n't": "won\\'t",
+        r"ca n't": "can\\'t",
     }
 
     # Punctuation
@@ -104,13 +105,13 @@ def make_sentence_human_readable_new(sentence: str) -> str:
 
 
 def main():
-    test_sentence = r"We 've been swimming for hours just to get to the other side . A big fish was swimming in the tank . Do n't fire until you see the whites of their eyes . The gun fired ."
+    test_sentence = r"My boss is out on another of his three martini lunches . Will you join us at six o'clock for martinis ? We 've been swimming for hours just to get to the other side . A big fish was swimming in the tank . Do n't fire until you see the whites of their eyes . The gun fired ."
 
     print("Using NEW algorithm (default):")
-    print(make_sentence_human_readable(test_sentence))
+    print(make_sentence_human_readable(test_sentence)) # My boss is out on another of his three martini lunches.	Will you join us at six o\'clock for martinis?
 
     print("\nUsing OLD algorithm:")
-    print(make_sentence_human_readable(test_sentence, version="old"))
+    print(make_sentence_human_readable(test_sentence, version="old")) # My boss is out on another of his three martini lunches.	Will you join us at six o\'clock for martinis?
 
 
 if __name__ == '__main__':
