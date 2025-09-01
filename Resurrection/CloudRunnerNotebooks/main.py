14b495508bfe23b3dab7d3116bd163d04f19d923
Fabian Bernat <fabbernat@gmail.com>
2025-09-01 20:37:27 +0200

    v9.0: I DID IT! It seems like I got a valid, 78% answer on Gemini-Flash-Thinking Pro

---
+++ Resurrection/CloudRunnerNotebooks/main.py
@@ -0,0 +1,14 @@
+MODEL_NAME =  '                   '
+
+MODEL_NAME = MODEL_NAME.strip()
+
+class ChatGPT:
+    def ask(self, question: str) -> str:
+
+def main():
+    prompt = open('prompt.txt').read()
+    chatGPT: ChatGPT = ChatGPT()
+    chatGPT.ask(prompt)
+
+if __name__ == '__main__':
+    main()
