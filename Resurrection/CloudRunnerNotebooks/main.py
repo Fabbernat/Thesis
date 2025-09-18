38317c7b8719226cf8908ce52752bcd1663e9837
Fabian Bernat <fabbernat@gmail.com>
2025-09-18 17:21:03 +0200

    v9.3.3: in theory I think I coded the full process of running Qwen/Qwen2.5-0.5B-Instruct

--- Resurrection/CloudRunnerNotebooks/main.py
+++ Resurrection/CloudRunnerNotebooks/main.py
@@ -4,6 +4,8 @@ from Resurrection.CloudRunnerNotebooks.run import run
 
 def main():
     run()
-
+'''
+requires pip install torch, transformers, accelerate
+'''
 if __name__ == '__main__':
     main()
