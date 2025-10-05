33cd607479359d98725f44979a0e9c2dbf80a919
Fabian Bernat <fabbernat@gmail.com>
2025-10-05 23:52:11 +0200

    v9.5.2: update outdated model name

--- Resurrection/HuggingFaceModelInferencer/Config/Config.py
+++ Resurrection/HuggingFaceModelInferencer/Config/Config.py
@@ -1,5 +1,5 @@
 tested_models = ['Qwen/Qwen2.5-0.5B-Instruct', 'Qwen/Qwen2.5-0.5B'] # tested models, that will grantedly work
-untested_models = ['google/gemma-2b'] # for your own responsibility
+untested_models = ['google/gemma-2-2b'] # for your own responsibility
 MODEL_NAME =  untested_models[0]
 FILE_NAME = "prompt.txt"
 DEVICE = "cuda"
