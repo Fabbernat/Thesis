7221436ebb5cdb3390931c05f74d32d0e882bc84
fabbernat <fabbernat@gmail.com>
2025-10-15 12:15:57 +0200

    v9.5.6: Qwen/Qwen2.5-0.5B runs well too, in this version

--- Resurrection/HuggingFaceModelInferencer/Config/Config.py
+++ Resurrection/HuggingFaceModelInferencer/Config/Config.py
@@ -1,6 +1,6 @@
-tested_models = ['Qwen/Qwen2.5-0.5B-Instruct', 'Qwen/Qwen2.5-0.5B'] # tested models, that will grantedly work
+tested_models = ['Qwen/Qwen2.5-0.5B-Instruct', 'Qwen/Qwen2.5-0.5B', 'Qwen/Qwen2.5-1.5B'] # tested models, that will grantedly work
 untested_models = ['google/gemma-2-2b'] # for your own responsibility
-MODEL_NAME =  tested_models[0]
+MODEL_NAME =  tested_models[2]
 FILE_NAME = "prompt.txt"
 DEVICE = "cuda"
 NUMBER_OF_DESIRED_ANSWERS = 100
