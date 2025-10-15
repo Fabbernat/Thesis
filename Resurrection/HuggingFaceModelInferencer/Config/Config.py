cbef0641b2fe00fd937cf36af3be43751ff27d94
fabbernat <fabbernat@gmail.com>
2025-10-15 17:05:42 +0200

    v9.6.1: convert Config.py list to dict

--- Resurrection/HuggingFaceModelInferencer/Config/Config.py
+++ Resurrection/HuggingFaceModelInferencer/Config/Config.py
@@ -1,18 +1,16 @@
-tested_models = [
-    'Qwen/Qwen2.5-0.5B-Instruct', 'Qwen/Qwen2.5-0.5B', 'Qwen/Qwen2.5-1.5B', 'Qwen/Qwen2.5-1.5B-Instruct',
-    'Qwen/Qwen2.5-3B', 'Qwen/Qwen2.5-3B-Instruct', 'Qwen/Qwen2.5-7B', 'Qwen/Qwen2.5-7B-Instruct', 'Qwen/Qwen2.5-14B', 'Qwen/Qwen2.5-14B-Instruct',
-    'Qwen/Qwen2.5-32B', 'Qwen/Qwen2.5-32B-Instruct', 'Qwen/Qwen2.5-72B', 'Qwen/Qwen2.5-72B-Instruct',
-    'Qwen/Qwen2.5-0.5B-Instruct-GGUF', 'Qwen/Qwen2.5-0.5B-Instruct-AWQ', 'Qwen/Qwen2.5-0.5B-Instruct-GPTQ-Int4',
-    'Qwen/Qwen2.5-0.5B-Instruct-GPTQ-Int8', 'Qwen/Qwen2.5-1.5B-Instruct-GGUF', 'Qwen/Qwen2.5-1.5B-Instruct-AWQ',
-    'Qwen/Qwen2.5-1.5B-Instruct-GPTQ-Int4', 'Qwen/Qwen2.5-1.5B-Instruct-GPTQ-Int8', 'Qwen/Qwen2.5-3B-Instruct-GGUF',
-    'Qwen/Qwen2.5-3B-Instruct-AWQ', 'Qwen/Qwen2.5-3B-Instruct-GPTQ-Int4', 'Qwen/Qwen2.5-3B-Instruct-GPTQ-Int8',
-    'Qwen/Qwen2.5-7B-Instruct-GGUF', 'Qwen/Qwen2.5-7B-Instruct-AWQ', 'Qwen/Qwen2.5-7B-Instruct-GPTQ-Int4',
-    'Qwen/Qwen2.5-7B-Instruct-GPTQ-Int8', 'Qwen/Qwen2.5-14B-Instruct-GGUF', 'Qwen/Qwen2.5-14B-Instruct-AWQ',
-    'Qwen/Qwen2.5-14B-Instruct-GPTQ-Int4', 'Qwen/Qwen2.5-14B-Instruct-GPTQ-Int8', 'Qwen/Qwen2.5-32B-Instruct-GGUF',
-    'Qwen/Qwen2.5-32B-Instruct-AWQ', 'Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4', 'Qwen/Qwen2.5-32B-Instruct-GPTQ-Int8',
-    'Qwen/Qwen2.5-72B-Instruct-GGUF', 'Qwen/Qwen2.5-72B-Instruct-AWQ', 'Qwen/Qwen2.5-72B-Instruct-GPTQ-Int4',
-    'Qwen/Qwen2.5-72B-Instruct-GPTQ-Int8'
-]  # tested models, that will grantedly work
+tested_models = {
+    0: 'Qwen/Qwen2.5-0.5B-Instruct', 2:'Qwen/Qwen2.5-0.5B', 3:'Qwen/Qwen2.5-1.5B', 4:'Qwen/Qwen2.5-1.5B-Instruct',
+5:'Qwen/Qwen2.5-3B',6: 'Qwen/Qwen2.5-3B-Instruct',7: 'Qwen/Qwen2.5-7B',8: 'Qwen/Qwen2.5-7B-Instruct',
+9:'Qwen/Qwen2.5-14B',10: 'Qwen/Qwen2.5-14B-Instruct',11: 'Qwen/Qwen2.5-32B',12: 'Qwen/Qwen2.5-32B-Instruct',
+13:'Qwen/Qwen2.5-72B',14: 'Qwen/Qwen2.5-72B-Instruct',15: 'Qwen/Qwen2.5-0.5B-Instruct-GGUF',16: 'Qwen/Qwen2.5-0.5B-Instruct-AWQ',
+17:'Qwen/Qwen2.5-0.5B-Instruct-GPTQ-Int4',18: 'Qwen/Qwen2.5-0.5B-Instruct-GPTQ-Int8', 19:'Qwen/Qwen2.5-1.5B-Instruct-GGUF',20: 'Qwen/Qwen2.5-1.5B-Instruct-AWQ',
+21:'Qwen/Qwen2.5-1.5B-Instruct-GPTQ-Int4',22: 'Qwen/Qwen2.5-1.5B-Instruct-GPTQ-Int8',23: 'Qwen/Qwen2.5-3B-Instruct-GGUF',24: 'Qwen/Qwen2.5-3B-Instruct-AWQ',
+25:'Qwen/Qwen2.5-3B-Instruct-GPTQ-Int4',26: 'Qwen/Qwen2.5-3B-Instruct-GPTQ-Int8', 27:'Qwen/Qwen2.5-7B-Instruct-GGUF',28: 'Qwen/Qwen2.5-7B-Instruct-AWQ',
+29:'Qwen/Qwen2.5-7B-Instruct-GPTQ-Int4',30: 'Qwen/Qwen2.5-7B-Instruct-GPTQ-Int8',31: 'Qwen/Qwen2.5-14B-Instruct-GGUF',32: 'Qwen/Qwen2.5-14B-Instruct-AWQ',
+33:'Qwen/Qwen2.5-14B-Instruct-GPTQ-Int4',34: 'Qwen/Qwen2.5-14B-Instruct-GPTQ-Int8',35: 'Qwen/Qwen2.5-32B-Instruct-GGUF',36: 'Qwen/Qwen2.5-32B-Instruct-AWQ',
+37:'Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4', 38:'Qwen/Qwen2.5-32B-Instruct-GPTQ-Int8', 39:'Qwen/Qwen2.5-72B-Instruct-GGUF', 40:'Qwen/Qwen2.5-72B-Instruct-AWQ',
+41:'Qwen/Qwen2.5-72B-Instruct-GPTQ-Int4',1: 'Qwen/Qwen2.5-72B-Instruct-GPTQ-Int8'
+}  # tested models, that will grantedly work
 untested_models = ['google/gemma-2-2b']  # for your own responsibility
 MODEL_NAME = tested_models[10]
 FILE_NAME = "prompt.txt"
