59aecf037b9f6786c81ea49f0cfd6a7733892678
Fabian Bernat <fabbernat@gmail.com>
2025-10-27 18:17:39 +0100

    v10.1: tried to improve model choice detection in ModelOutputProcessor main.py but failed so far.

--- src/Framework/ModelOutputProcessor/main.py
+++ src/Framework/ModelOutputProcessor/main.py
@@ -3,11 +3,25 @@ from src.Framework.ModelOutputProcessor.run import run
 TESTFILE_LENGTH = 1400
 
 # --- CONFIG ---
-AFFIRMATIVE_KEYWORDS = ['Yes']
-NEGATIVE_KEYWORDS = ['No']
+
+keywordPairs = {
+    "yes": ["Yes", "No", ],
+    "yes.": ["Yes.", "No."],
+    "arrowedYes": ["->Yes", "->No"],
+    "t": ["T", "F"],
+    "true": ["True", "False"],
+    "true.": ["True.", "False"],
+}
+
+AFFIRMATIVE_KEYWORDS = [keywordPairs["yes"][0], keywordPairs["yes."][0]]
+print(AFFIRMATIVE_KEYWORDS)
+NEGATIVE_KEYWORDS = [keywordPairs["yes"][1], keywordPairs["yes."][1]]
+print(NEGATIVE_KEYWORDS)
 
 AFFIRMATIVE_PHRASES = ['Of Course']
 NEGATIVE_PHRASES = ['Not at all']
+
+
 # --- end of config ---
 
 def main():
