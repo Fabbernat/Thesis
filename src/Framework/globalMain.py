e4a62e50b062a6aa0e526e3a32867242e77f2343
Fabian Bernat <fabbernat@gmail.com>
2025-11-03 23:29:16 +0100

    v10.4.4: simplify globalMain.py imports

--- src/Framework/globalMain.py
+++ src/Framework/globalMain.py
@@ -1,11 +1,12 @@
+from Framework import ModelInputPreparer, HuggingFaceModelInferencer, ModelOutputProcessor
 
 
 # BETA: Optional global main running all three submodules if you don't want to click three times (not recommended, since the second module often runs to errors or bugs)
 def main():
     try:
-        src.Framework.ModelInputPreparer.main.main()
-        src.Framework.HuggingFaceModelInferencer.main.main()
-        src.Framework.ModelOutputProcessor.main.main()
+        ModelInputPreparer.main.main()
+        HuggingFaceModelInferencer.main.main()
+        ModelOutputProcessor.main.main()
     except AttributeError as ae:
         print('This is not working. Run the three modules one by one instead. Error:', ae)
 
