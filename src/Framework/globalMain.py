3a6048022a9421bd8432190332eb06fedb9e0b38
fabbernatvasvari <sze4fabber@vasvari.org>
2025-11-03 16:10:56 +0100

    last try

--- src/Framework/globalMain.py
+++ src/Framework/globalMain.py
@@ -1,23 +1,29 @@
 CONNECTED = False
 try:
-    from src.Framework.ModelInputPreparer import main as m1
-    from src.Framework.HuggingFaceModelInferencer import main as m2
-    from src.Framework.ModelOutputProcessor import main as m3
+    from src.Framework.ModelInputPreparer.main import main as m1
+    from src.Framework.HuggingFaceModelInferencer.main import main as m2
+    from src.Framework.ModelOutputProcessor.main import main as m3
 except ModuleNotFoundError as mne:
     print('ModuleNotFoundError', mne)
-    from .ModelInputPreparer import main as m1
-    from .HuggingFaceModelInferencer import main as m2
-    from .ModelOutputProcessor import main as m3
+
+try:
+    from .ModelInputPreparer.main import main as m1
+    from .HuggingFaceModelInferencer.main import main as m2
+    from .ModelOutputProcessor.main import main as m3
+except ImportError as ie:
+    print('ImportError', ie)
 
 # BETA: Optional global main running all three submodules if you don't want to click three times (not recommended, since the second module often runs to errors or bugs)
 def main():
     CONNECTED = True
     try:
-        m1.main()
-        m2.main()
-        m3.main()
+        m1()
+        m2()
+        m3()
     except AttributeError as ae:
         print('This is not working. Run the three modules one by one instead. Error:', ae)
+    except NameError as ne:
+        print('NameError', ne)
 
 if __name__ == "__main__":
     main()
