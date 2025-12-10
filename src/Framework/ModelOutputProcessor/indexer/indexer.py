f4dc0f6d253f1349eda4481945228c87344b54b7
fabbernatvasvari <sze4fabber@vasvari.org>
2025-12-10 13:38:55 +0100

    v10.8.3.12: 100/200 save before running on google

--- src/Framework/ModelOutputProcessor/indexer/indexer.py
+++ src/Framework/ModelOutputProcessor/indexer/indexer.py
@@ -1,24 +1,37 @@
-def indexer(straightAnswers, reversedAnswers, guids, goldStandard: list[bool]):
+def indexer(straightAnswers: dict[str, str],
+            reversedAnswers: dict[str, str],
+            guids: list[int],
+            goldStandard: list[bool]):
     """"""
+    keys = list(straightAnswers.keys())
 
-    if len(straightAnswers) != len(reversedAnswers) or len(straightAnswers) != len(guids) or len(reversedAnswers) != len(guids):
+    if len(straightAnswers) != len(reversedAnswers) or len(straightAnswers) != len(guids) or len(
+            reversedAnswers) != len(guids):
         import sys
-        sys.stderr.write("Error: the lengths of the straight and reversed questions, as well as the GUIDs, must match! Do you wish to continue? (y/n)\n")
+        sys.stderr.write(
+            "Error: the lengths of the straight and reversed questions, as well as the GUIDs, must match! Do you wish to continue? (y/n) ")
         key = input()
         if key.lower().strip() == 'n':
             exit(0)
 
-
-
-    length = min(len(straightAnswers), len(reversedAnswers), len(guids), len(goldStandard)) # elvileg mindegyiknek ugyanolyan hosszúnak kéne lennie, de a gyakorlatban általában nem az.
+    length = min(len(straightAnswers), len(reversedAnswers), len(guids),
+                 len(goldStandard))  # elvileg mindegyiknek ugyanolyan hosszúnak kéne lennie, de a gyakorlatban általában nem az.
     consistents = accurates = consistentlyAccurates = ambiguouses = consistentlyAmbiguouses = 0
 
     for i in range(length):
 
+        key = keys[i]  # pl. 'sound'
 
-        token1 = straightAnswers[i]
-        token2 = reversedAnswers[i]
-        groundTruth = goldStandard[guids[i]]
+        token1 = straightAnswers.get(key, None)
+        if token1 is None:
+            ambiguouses += 1
+            token1 = '?'
+        token2 = reversedAnswers.get(key, None)
+        if token2 is None:
+            ambiguouses += 1
+            token2 = '?'
+
+        groundTruth = goldStandard[guids[i]]  # egész szám index a goldStandard listához
 
         valid = {'Yes', 'No'}
         if token1 not in valid:
@@ -34,13 +47,19 @@ def indexer(straightAnswers, reversedAnswers, guids, goldStandard: list[bool]):
             consistents += 1
             consistentlyAmbiguouses += 1
 
-        if token1 == 'Yes' and groundTruth:
-            accurates += 1
-        if token2 == 'Yes' and groundTruth:
-            accurates += 1
-        if token1 == 'No' and not groundTruth:
-            accurates += 1
-        if token2 == 'No' and not groundTruth:
+        # helyes-e a straight válasz?
+        straightCorrect = (
+                (token1 == 'Yes' and groundTruth) or
+                (token1 == 'No' and not groundTruth)
+        )
+
+        # helyes-e a reversed válasz?
+        reversedCorrect = (
+                (token2 == 'Yes' and groundTruth) or
+                (token2 == 'No' and not groundTruth)
+        )
+
+        if straightCorrect or reversedCorrect:
             accurates += 1
 
         if token1 == 'Yes' and token2 == 'Yes' and groundTruth:
@@ -48,21 +67,95 @@ def indexer(straightAnswers, reversedAnswers, guids, goldStandard: list[bool]):
         if token1 == 'No' and token2 == 'No' and not groundTruth:
             consistentlyAccurates += 1
 
-
-    return consistents / length, accurates / length, consistentlyAccurates / length, ambiguouses / length, consistentlyAmbiguouses / length # 0 és 1 közé normalizáljuk
+    return consistents / length, accurates / length, consistentlyAccurates / length, ambiguouses / length, consistentlyAmbiguouses / length  # 0 és 1 közé normalizáljuk
 
 
 def test_indexer():
-    straightAnswers: dict[str, str] = {'sound': 'Yes','grow': 'No', 'audience':'No','insufficiency': 'No','batch': 'No','extent': 'No','agency': 'No','narcolepsy': 'No','score': 'Yes','instill': 'No','amount': 'No','generation': 'No','vagina': 'No','guard': 'No','allowance': 'No','site': 'No','eclat': 'No','compel': 'No','inwardness': 'Yes','height': 'No','fall': 'No','obstruction': 'Yes','agony': 'No','palpitate': 'No','logic': 'No','suspect': 'No','analyze': 'No','repair': 'No','stampede': 'No','retroversion': 'No','exploit': 'No','correct': 'No','shade': 'No','heat': 'Yes','demonstration': 'No','explode': 'No','mound': 'No','nursing': 'No','repression': 'No', 'ice':'No','lubricate': 'No','strain': 'No','construction': 'No','mate': 'No','sewer': 'No','origin': 'No','manner': 'No','model': 'No','bank': 'No'}
-    reversedAnswers: dict[str, str] = {'sound':'No','grow': 'No','audience': 'No','insufficiency': 'No','batch': 'No','extent': 'No','extract': 'No','agency': 'No', 'narcolepsy':'Yes','score': 'No','instill': 'No','amount': 'No','generation': 'No','vagina':
-                                  'No', 'guard':'No','allowance': 'No','site': 'No','eclat': 'No', 'compel':'No','inwardness': 'No','height': 'No','fall': 'No', 'obstruction':'No','agony': 'No','palpitate': 'No','logic': 'No',
-                                 'suspect': 'No','analyze': 'No','repair': 'No','stampede': 'No', 'retroversion':'No','exploit': 'No','correct': 'No','shade': 'Yes','heat': 'No','demonstration': 'No','explode': 'No','mound': 'No','nursing': 'No',
-                                  'repression':'No','ice': 'No','lubricate': 'Yes','strain': 'No', 'construction':'No','mate': 'No','sewer': 'No','origin': 'No', 'manner':'No', 'model': 'No','bank': 'No'}
-    guids: list[int] = [964, 398, 22, 1282, 516, 902, 489, 911, 591, 1244, 1389, 178, 1291, 10, 355, 596, 615, 103, 1359, 58, 515, 1316, 468, 456, 1005, 31, 879, 303, 1315, 243, 1112, 847, 204, 1328, 1268, 1092, 1127, 291, 1188, 1323, 212, 80, 1357, 1003, 564, 659, 214, 894, 863, 834, 508, 715, 735, 361, 312, 435, 1256, 583, 1194, 702, 1145, 788, 20, 1286, 907, 438, 1351, 935, 251, 1327, 85, 250, 1072, 461, 665, 932, 304, 1107, 1214, 337, 368, 794, 12, 298, 988, 512, 895, 660, 380, 1067, 770, 687, 169, 268, 345, 1118, 388, 102, 810, 1028]
+    straightAnswers: dict[str, str] = {
+        'sound': 'Yes', 'grow': 'No', 'audience': 'No',
+        'insufficiency': 'No',
+        'batch': 'No', 'extent': 'No','extract':'No' , 'agency': 'No', 'narcolepsy': 'No',
+        'score': 'Yes', 'instill': 'No', 'amount': 'No', 'generation': 'No',
+        'vagina': 'No', 'guard': 'No', 'allowance': 'No', 'site': 'No', 'eclat': 'No',
+        'compel': 'No', 'inwardness': 'Yes', 'height': 'No', 'fall': 'No',
+        'obstruction': 'Yes', 'agony': 'No', 'palpitate': 'No', 'logic': 'No',
+        'suspect': 'No', 'analyze': 'No', 'repair': 'No', 'stampede': 'No',
+        'retroversion': 'No', 'exploit': 'No', 'correct': 'No', 'shade': 'No',
+        'heat': 'Yes', 'demonstration': 'No', 'explode': 'No', 'mound': 'No',
+        'nursing': 'No', 'repression': 'No', 'ice': 'No', 'lubricate': 'No',
+        'strain': 'No', 'construction': 'No', 'mate': 'No', 'sewer': 'No',
+        'origin': 'No', 'manner': 'No', 'model': 'No', 'bank': 'No'}
+    reversedAnswers: dict[str, str] = {
+        'sound': 'No', 'grow': 'No', 'audience': 'No', 'insufficiency': 'No',
+        'batch': 'No', 'extent': 'No', 'extract': 'No', 'agency': 'No',
+        'narcolepsy': 'Yes', 'score': 'No', 'instill': 'No', 'amount': 'No',
+        'generation': 'No', 'vagina':
+            'No', 'guard': 'No', 'allowance': 'No', 'site': 'No', 'eclat': 'No',
+        'compel': 'No', 'inwardness': 'No', 'height': 'No', 'fall': 'No',
+        'obstruction': 'No', 'agony': 'No', 'palpitate': 'No', 'logic': 'No',
+        'suspect': 'No', 'analyze': 'No', 'repair': 'No', 'stampede': 'No',
+        'retroversion': 'No', 'exploit': 'No', 'correct': 'No', 'shade': 'Yes',
+        'heat': 'No', 'demonstration': 'No', 'explode': 'No', 'mound': 'No',
+        'nursing': 'No',
+        'repression': 'No', 'ice': 'No', 'lubricate': 'Yes', 'strain': 'No',
+        'construction': 'No', 'mate': 'No', 'sewer': 'No', 'origin': 'No',
+        'manner': 'No', 'model': 'No', 'bank': 'No'}
+    guids: list[int] = [
+        137,
+        399,
+        1041,
+        1173,
+        910,
+        949,
+        866,
+        967,
+        498,
+        170,
+        1110,
+        1016,
+        1212,
+        378,
+        755,
+        42,
+        344,
+        1020,
+        1019,
+        689,
+        956,
+        1127,
+        520,
+        948,
+        306,
+        89,
+        1278,
+        773,
+        1387,
+        196,
+        337,
+        476,
+        1292,
+        451,
+        462,
+        625,
+        1295,
+        1064,
+        1391,
+        1090,
+        744,
+        113,
+        1290,
+        429,
+        29,
+        761,
+        780,
+        616,
+        1213,
+        204
+    ]
     print(len(straightAnswers), len(reversedAnswers), len(guids))
 
     goldStandard: list[bool] = []
-    lines:list[str] = []
+    lines: list[str] = []
 
     from pathlib import Path
     basePath = Path(__file__).resolve()
@@ -70,10 +163,11 @@ def test_indexer():
     with open(basePath.parents[1] / 'data' / 'test.gold.in', 'r', encoding='utf-8') as goldFile:
         lines = goldFile.readlines()
 
-    goldStandard = [line.strip() == 'T' for line in lines]
+    goldStandard = [line.strip() == 'T' for line in lines] # biztosítja, hogy bool legyen, True ha 'T', egyébként False
 
-    
-    consistents, accurates, consistentlyAccurates, ambiguouses, consistentlyAmbiguouses = indexer(straightAnswers, reversedAnswers, guids, goldStandard)
+    consistents, accurates, consistentlyAccurates, ambiguouses, consistentlyAmbiguouses = indexer(straightAnswers,
+                                                                                                  reversedAnswers,
+                                                                                                  guids, goldStandard)
 
     here = Path(__file__).resolve().parent
 
@@ -89,11 +183,15 @@ def test_indexer():
         printEverywhere(f'Ambiguous: {ambiguouses * 100:.2f} %', f)
         printEverywhere(f'Consistently ambiguous: {consistentlyAmbiguouses * 100:.2f} %', f)
 
+
 def printEverywhere(msg: str, file):
     print(msg)
     file.write(msg + "\n")
 
+
 def main():
     test_indexer()
+
+
 if __name__ == '__main__':
     main()
